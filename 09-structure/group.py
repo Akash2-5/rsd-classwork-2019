bdb11f0a16ac504d54d1629c7a35cb558ba7d0b1
Akash Patel <akash.2@outlook.com>
2019-12-05 11:28:33 +0000

    Removed global variables

--- 09-structure/group.py
+++ 09-structure/group.py
@@ -32,8 +32,7 @@ group = {
 }
 
 
-def average_age():
-    """Compute the average age of the group's members."""
+def average_age(group):
     all_ages = [person["age"] for person in group.values()]
     return sum(all_ages) / len(group)
 
@@ -55,7 +54,39 @@ def add_person(name, age, job, relations):
 
 
 if __name__ == "__main__":
+    group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
     print("The group has {} members with an average age of {}".format(
         len(group),
-        average_age()
+        average_age(group)
     ))
